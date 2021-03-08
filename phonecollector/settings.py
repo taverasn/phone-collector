6d3d76b6a52639846f8c20e93d0181837372b631
Nicholas Taveras III <taveras123n@gmail.com>
2021-03-08 16:01:21 -0500

    Config Deployment

--- phonecollector/settings.py
+++ phonecollector/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,3 +129,6 @@ LOGIN_REDIRECT_URL = '/phones/'
 LOGOUT_URL = '/accounts/logout/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
